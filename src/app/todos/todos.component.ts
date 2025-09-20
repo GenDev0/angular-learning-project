043b8bff0b63b7ca9dfa84a9dc218b0345522338
GenDev0 <dev.Gen.info0@gmail.com>
2025-09-20 21:57:33 +0100

    Making HTTP calls In Angular service

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { TodosService } from '../services/todos.service';
+import { Todo } from '../model/todo.type';
+import { CommonModule } from '@angular/common';
+import { catchError, filter, map, take } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.scss'
+  styleUrl: './todos.component.scss',
 })
-export class TodosComponent {
-
+export class TodosComponent implements OnInit {
+  todoService = inject(TodosService);
+  todoItems = signal<Array<Todo>>([]);
+  ngOnInit(): void {
+    this.todoService
+      .getTodosFromApi()
+      .pipe(
+        // map(
+        //   (todos) => todos.filter((todo) => !todo.completed).slice(0, 5) // filter + take first 5
+        // ),
+        catchError((err) => {
+          console.error('Error fetching todos', err);
+          throw err;
+        })
+      )
+      .subscribe((todos) => {
+        this.todoItems.set(todos);
+      });
+  }
 }
